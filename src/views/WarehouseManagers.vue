0d2ed7370f547327fb009576790ab1f22676931d
Autzoko <llt020725@163.com>
2024-03-26 22:38:20 +0800

    developing add manager fyunctions

--- src/views/WarehouseManagers.vue
+++ src/views/WarehouseManagers.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
       <h1>Manager Information</h1>
+      <el-button type="primary" @click="showAddManagerDialog">Add Manager</el-button>
       <el-table :data="managers" style="width: 100%">
           <el-table-column prop="manager_id" label="ID"></el-table-column>
           <el-table-column prop="manager_name" label="Name"></el-table-column>
@@ -9,16 +10,48 @@
           <el-table-column prop="manager_email" label="Email"></el-table-column>
           <el-table-column prop="last_login_datetime" label="Last Login"></el-table-column>
       </el-table>
+
+      <el-dialog title="New Manager Information" :visible.sync="addManagerDialogVisible">
+          <el-form :model="newManager" label-width="80px">
+              <el-form-item label="ID">
+                  <el-input v-model="newManager.manager_id" placeholder="Enter ID"></el-input>
+              </el-form-item>
+              <el-form-item label="Name">
+                  <el-input v-model="newManager.manager_name" placeholder="Enter Name"></el-input>
+              </el-form-item>
+              <el-form-item label="Password">
+                  <el-input v-model="newManager.manager_pwd" placeholder="Enter Password"></el-input>
+              </el-form-item>
+              <el-form-item label="Level">
+                  <el-input v-model="newManager.manager_level" placeholder="Set Level"></el-input>
+              </el-form-item>
+              <el-form-item label="Last Login">
+                  <el-input v-model="newManager.last_login_datetime" placeholder="Enter Time"></el-input>
+              </el-form-item>
+              <el-form-item label="Email">
+                  <el-input v-model="newManager.manager_email" placeholder="Enter Email"></el-input>
+              </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+              <el-button @click="cancelAddManager">Cancel</el-button>
+              <el-button type="primary" @click="saveNewManager">Save</el-button>
+          </div>
+      </el-dialog>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import { ref } from 'vue'
+import {useRouter} from "vue-router";
+
 export default {
     name: "WarehouseManagers",
     components: {},
     data() {
         return {
+            newManager: {},
+            addManagerDialogVisible: false,
             managers: []
         }
     },
@@ -32,6 +65,23 @@ export default {
                 this.managers = response.data
             } catch (error) {
                 console.error('Error fetching manager data: ', error)
+            }
+        },
+        showAddManagerDialog() {
+            this.newManager = {};
+            this.addManagerDialogVisible = true;
+        },
+        cancelAddManager() {
+            this.newManager = {};
+            this.addManagerDialogVisible = false;
+        },
+        async saveNewManager() {
+            try {
+                await axios.post('api/warehouse-manager/add-manager', this.newManager);
+                this.addManagerDialogVisible = false;
+                this.fetchManagers();
+            } catch (error) {
+                console.error('Error adding manager: ', error);
             }
         }
     }
