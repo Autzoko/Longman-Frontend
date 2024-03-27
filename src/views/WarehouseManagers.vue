fa352e0eb3025788aa4a6584381b9ee5b84e2021
Autzoko <llt020725@163.com>
2024-03-27 17:03:09 +0800

    CRUD for manager basically finished

--- src/views/WarehouseManagers.vue
+++ src/views/WarehouseManagers.vue
@@ -1,35 +1,48 @@
 <template>
-  <div>
-      <h1>Manager Information</h1>
-      <el-button type="primary" @click="showAddManagerDialog">Add Manager</el-button>
-      <el-table :data="managers" style="width: 100%">
+  <div style="margin: 20px">
+      <h1 class="title">Manager Information</h1>
+      <el-button type="primary" @click="showAddManagerDialog" style="margin-bottom: 20px">Add Manager</el-button>
+      <el-table :data="managers" style="width: 100%" border>
           <el-table-column prop="manager_id" label="ID"></el-table-column>
           <el-table-column prop="manager_name" label="Name"></el-table-column>
           <el-table-column prop="manager_pwd" label="Password"></el-table-column>
           <el-table-column prop="manager_level" label="Level"></el-table-column>
           <el-table-column prop="manager_email" label="Email"></el-table-column>
           <el-table-column prop="last_login_datetime" label="Last Login"></el-table-column>
+          <el-table-column label="Actions">
+              <template #default="{row}">
+                  <el-button type="text" @click="openEditDialog(row)">Edit</el-button>
+                  <el-button type="text" @click="deleteManager(row)">Delete</el-button>
+              </template>
+          </el-table-column>
       </el-table>
 
-      <el-dialog title="New Manager Information" :visible.sync="addManagerDialogVisible">
+      <el-dialog title="New Manager Information" v-model="addManagerDialogVisible" width="50%">
           <el-form :model="newManager" label-width="80px">
-              <el-form-item label="ID">
-                  <el-input v-model="newManager.manager_id" placeholder="Enter ID"></el-input>
+              <el-form-item label="ID" required>
+                  <el-input v-model="newManager.manager_id" placeholder="Enter ID" style="margin-left: 30px"></el-input>
               </el-form-item>
-              <el-form-item label="Name">
-                  <el-input v-model="newManager.manager_name" placeholder="Enter Name"></el-input>
+              <el-form-item label="Name" required>
+                  <el-input v-model="newManager.manager_name" placeholder="Enter Name" style="margin-left: 30px"></el-input>
               </el-form-item>
-              <el-form-item label="Password">
-                  <el-input v-model="newManager.manager_pwd" placeholder="Enter Password"></el-input>
+              <el-form-item label="Password" required>
+                  <el-input v-model="newManager.manager_pwd" placeholder="Enter Password" style="margin-left: 30px"></el-input>
               </el-form-item>
-              <el-form-item label="Level">
-                  <el-input v-model="newManager.manager_level" placeholder="Set Level"></el-input>
+              <el-form-item label="Level" required>
+                  <el-select v-model="newManager.manager_level" placeholder="Select Level" style="margin-left: 30px">
+                      <el-option v-for="level in levels" :key="level.value" :label="level.label" :value="level.value"></el-option>
+                  </el-select>
               </el-form-item>
-              <el-form-item label="Last Login">
-                  <el-input v-model="newManager.last_login_datetime" placeholder="Enter Time"></el-input>
+              <el-form-item label="Log Info" required label-width="80px">
+                  <el-date-picker
+                      v-model="newManager.last_login_datetime"
+                      type="datetime"
+                      placeholder="Select Date and Time"
+                      :default-value="new Date()"
+                      style="margin-left: 30px"></el-date-picker>
               </el-form-item>
               <el-form-item label="Email">
-                  <el-input v-model="newManager.manager_email" placeholder="Enter Email"></el-input>
+                  <el-input v-model="newManager.manager_email" placeholder="Enter Email" style="margin-left: 30px"></el-input>
               </el-form-item>
           </el-form>
           <div slot="footer" class="dialog-footer">
@@ -37,22 +50,61 @@
               <el-button type="primary" @click="saveNewManager">Save</el-button>
           </div>
       </el-dialog>
+
+      <el-dialog title="Edit Manager Information" v-model="editManagerDialogVisible" width="50%">
+          <el-form :model="updateManagerForm" label-width="100px">
+              <el-form-item label="Select Field">
+                  <el-select v-model="selectedField" placeholder="Select a Field">
+                      <el-option v-for="field in fields" :key="field.value" :label="field.label" :value="field.value"></el-option>
+                  </el-select>
+              </el-form-item>
+              <el-form-item v-if="selectedField !== ''" :label="fields.find(field => field.value === selectedField).label">
+                  <el-input v-model="updateManagerForm[selectedField]" placeholder="Enter Value"></el-input>
+              </el-form-item>
+              <el-form-item v-if="selectedField === 'last_login_datetime'" :label="fields.find(field => field.value === selectedField).label">
+                  <el-date-picker v-model="updateManagerForm[selectedField]" type="datetime" placeholder="Select Date and Time" label="Log Info"></el-date-picker>
+              </el-form-item>
+              <el-form-item v-if="selectedField === 'manager_level'" :label="fields.find(field => field.value === selectedField).label">
+                  <el-select v-model="updateManagerForm[selectedField]" placeholder="Select Level">
+                      <el-option v-for="level in levels" :key="level.value" :label="level.label" :value="level.value"></el-option>
+                  </el-select>
+              </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+              <el-button @click="editManagerDialogVisible = false">Cancel</el-button>
+              <el-button type="primary" @click="updateManager">Update</el-button>
+          </div>
+      </el-dialog>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-import { ref } from 'vue'
-import {useRouter} from "vue-router";
 
 export default {
     name: "WarehouseManagers",
     components: {},
     data() {
         return {
+            managers: [],
             newManager: {},
             addManagerDialogVisible: false,
-            managers: []
+            editManagerDialogVisible: false,
+            updateManagerForm: {},
+            selectedField: '',
+            fields: [
+                {label: 'ID', value: 'manager_id'},
+                {label: 'Name', value: 'manager_name'},
+                {label: 'Password', value: 'manager_pwd'},
+                {label: 'Level', value: 'manager_level'},
+                {label: 'Log Info', value: 'last_login_datetime'},
+                {label: 'Email', value: 'manager_email'}
+            ],
+            levels: [
+                {label: 'Main', value: 'main'},
+                {label: 'Vice', value: 'vice'},
+                {label: 'Common', value: 'common'}
+            ]
         }
     },
     mounted() {
@@ -68,20 +120,53 @@ export default {
             }
         },
         showAddManagerDialog() {
-            this.newManager = {};
             this.addManagerDialogVisible = true;
         },
         cancelAddManager() {
-            this.newManager = {};
             this.addManagerDialogVisible = false;
+            this.newManager = {};
         },
         async saveNewManager() {
             try {
                 await axios.post('api/warehouse-manager/add-manager', this.newManager);
                 this.addManagerDialogVisible = false;
                 await this.fetchManagers();
-            } catch (error) {
-                console.error('Error adding manager: ', error);
+                this.$message.success("Success");
+            } catch(error) {
+                console.log("Error adding manager: ", error);
+                this.$message.error("Add Failed");
+            }
+        },
+        editManager(row) {
+            console.log('edit');
+        },
+        async deleteManager(row) {
+            try {
+                await axios.delete('api/warehouse-manager/delete-manager', {
+                    params: {
+                        managerID: row.manager_id
+                    }
+                });
+                await this.fetchManagers();
+                this.$message.success('Delete Success');
+            } catch(error) {
+                console.error('Error deleting manager: ', error);
+                this.$message.error('Delete Failed');
+            }
+        },
+        openEditDialog(row) {
+            this.updateManagerForm = {...row};
+            this.selectedField = '';
+            this.editManagerDialogVisible = true;
+        },
+        async updateManager() {
+            try {
+                const response = await axios.put('api/warehouse-manager/update-manager', this.updateManagerForm);
+                this.editManagerDialogVisible = false;
+                await this.fetchManagers();
+                this.$message.success("Edit Success");
+            } catch(error) {
+                this.$message.error("Edit Failed");
             }
         }
     }
@@ -89,5 +174,7 @@ export default {
 </script>
 
 <style scoped>
-
+    .title {
+        margin-bottom: 20px;
+    }
 </style>
